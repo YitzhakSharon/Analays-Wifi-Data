861fc432f53f9bb62f5b44d6bfdb7bd0dc863711
YitzhakSharon <yitzhak.sharon7@gmail.com>
2017-12-14 12:26:50 +0200

    javadoc

--- bin/README.md
+++
@@ -1,2 +0,0 @@
-# OOP
-this project will develop a system that collect geographic data , analyze it, and reprezentant them with graphic tools
--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,32 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		long a =System.currentTimeMillis();
+		/**
+		 * read wigle-wifi files and filter them by Scan
+		 */
 		FileCsv fe = new FileCsv();
 		fe.readForCsv("C:\\Users\\yitzhak\\Desktop\\boaz1");
 
+		/**
+		 * enter Database
+		 */
 		FileKml fl = new FileKml();
-
 		ArrayList<Scan> scan = fl.readFromCsv("Table.csv");
+		/**
+		 *   turn on the kml with filters
+		 */
+		Filter k = new Filter();
+		k.ChekFilterForKml(scan);
+
+		/**
+		 * turn on the Algoritem for calculate approximate location, and the csv files we want to check
+		 */
 		ArrayList<Scan> sample = fl.readFromCsv("C:\\Users\\yitzhak\\Desktop\\_comb_no_gps_ts2_.csv");
-		// Filter k = new Filter();
-		// k.ChekFilterForKml(arrayOfscan);
 		Algoritem temp1 = new Algoritem();
 		temp1.algo2tocsv(scan, sample);
 		temp1.algo1tocsv(scan);
+
+
 		long b= System.currentTimeMillis();
 		System.out.println(b-a);
 
