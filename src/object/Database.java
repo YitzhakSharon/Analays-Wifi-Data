0da55e58c1933085c5f24e873e0413bda833e486
meravboim <meravboim123@gmail.com>
2017-12-26 14:25:47 +0200

    database

--- src/object/Database.java
+++ src/object/Database.java
@@ -9,140 +9,160 @@ import java.util.TreeSet;
 
 public class Database {
 
-	private ArrayList<Scan>  database;
+	private ArrayList<Scan> database;
 	private Map<String, ArrayList<Scan>> hash_map;
+
 	/**
-	 * empty contractor 
+	 * empty contractor
 	 */
 	public Database() {
-		this.database=new ArrayList<Scan>();
-		this.hash_map=new HashMap<String, ArrayList<Scan>>();
+		this.database = new ArrayList<Scan>();
+		this.database.clear();
+		this.hash_map = new HashMap<String, ArrayList<Scan>>();
 	}
-	public Database(ArrayList<Scan > scan) {
-		this.database=new ArrayList<Scan>();
-		 this.database.addAll(scan);
-		this.hash_map=new HashMap<String, ArrayList<Scan>>();
-		for (int i = 0; i <scan.size(); i++) {
-			insertHash(scan.get(i));
-		}
+
+	public Database(ArrayList<Scan> scan) {
+		this.database = new ArrayList<Scan>();
+		this.database.clear();
+		this.database.addAll(scan);
+		this.hash_map = new HashMap<String, ArrayList<Scan>>();
+//		for (int i = 0; i < scan.size(); i++) {
+//			insertHash(scan.get(i));
+//		}
+		this.hash_map=hashmap();
+		System.out.println(this.hash_map.size());
 	}
 
 	/**
 	 * contractor
+	 * 
 	 * @param other
 	 */
-	public void  insertHash (Scan other) {
-	//	this.database=new ArrayList<Scan>();
+	public void insertHash(Scan other) {
+		// this.database=new ArrayList<Scan>();
 		for (int i = 0; i < other.getWifiNetWork(); i++) {
 			if (this.hash_map.containsKey(other.getWifi().get(i).getMAC()))
-				if(!this.hash_map.get(other.getWifi().get(i).getMAC()).contains(other))
-				this.hash_map.get(other.getWifi().get(i).getMAC()).add(other);
-			else {
-				ArrayList<Scan> temp = new ArrayList<Scan>();
-				temp.add(other);
-				this.hash_map.put(other.getWifi().get(i).getMAC(), temp);
-			}
+				if (!this.hash_map.get(other.getWifi().get(i).getMAC()).contains(other))
+					this.hash_map.get(other.getWifi().get(i).getMAC()).add(other);
+				else {
+					ArrayList<Scan> temp = new ArrayList<Scan>();
+					temp.add(other);
+					this.hash_map.put(other.getWifi().get(i).getMAC(), temp);
+				}
 
 		}
-		this.database.add(other);
 	}
+
 	/**
 	 * copy contractor
+	 * 
 	 * @param other
 	 */
-	public Database (Database other) {
-		this.database=new ArrayList<Scan>();
+	public Database(Database other) {
 		this.database.clear();
+		this.database = new ArrayList<Scan>();
 		this.database.addAll(other.database);
-		hashmap();
+		this.hash_map = hashmap();
 	}
+
 	/**
 	 * add ArrayList to the database
+	 * 
 	 * @param other
 	 */
-	public void addArrayList (ArrayList<Scan> other) {
+	public void addArrayList(ArrayList<Scan> other) {
 		this.database.addAll(other);
-		//douplicate();
-		for (int i = 0; i <other.size(); i++) {
+		douplicate();
+		for (int i = 0; i < other.size(); i++) {
 			insertHash(other.get(i));
 		}
-		//we need to update the hash map and check douplicat in the hash map
-		
+		// we need to update the hash map and check douplicat in the hash map
+
 	}
+
 	/**
 	 * add one Scan
+	 * 
 	 * @param other
 	 */
-	public void addScan (Scan other) {
-		if(!this.database.contains(other))
-		this.database.add(other);
-		//we need to update the hash map and check douplicat in the hash map
+	public void addScan(Scan other) {
+		if (!this.database.contains(other))
+			this.database.add(other);
+		// we need to update the hash map and check douplicat in the hash map
 
 	}
 
 	/**
 	 * clear the database
 	 */
-	public void cleardatabase () {
+	public void cleardatabase() {
 		this.database.clear();
 	}
 
 	/**
 	 * that method turn ArrayList of Scan to hashMap for algo2
+	 * 
 	 * @param scan
 	 * @return
 	 */
 
-	public void hashmap() {
-		this.hash_map = new HashMap<String, ArrayList<Scan>>();
+	public Map<String, ArrayList<Scan>> hashmap() {
+		Map<String, ArrayList<Scan>> find = new HashMap<String, ArrayList<Scan>>();
 		for (int i = 0; i < this.database.size(); i++) {
 			for (int j = 0; j < this.database.get(i).getWifi().size(); j++) {
-				if (this.hash_map.containsKey(this.database.get(i).getWifi().get(j).getMAC()))
-					this.hash_map.get(this.database.get(i).getWifi().get(j).getMAC()).add(this.database.get(i));
+				if (find.containsKey(this.database.get(i).getWifi().get(j).getMAC()))
+					find.get(this.database.get(i).getWifi().get(j).getMAC()).add(this.database.get(i));
 				else {
 					ArrayList<Scan> temp = new ArrayList<Scan>();
 					temp.add(this.database.get(i));
-					this.hash_map.put(this.database.get(i).getWifi().get(j).getMAC(), temp);
+					find.put(this.database.get(i).getWifi().get(j).getMAC(), temp);
 				}
 			}
 		}
+		return find;
 	}
 
 	public int numOfScan() {
 		return this.database.size();
 	}
+
 	public int numOfmacs() {
-		return this.hash_map.size();
+		Map<String, ArrayList<Scan>> find = hashmap();
+		return find.size();
 	}
+
 	/**
-	 * toString 
+	 * toString
 	 */
-	public String toString(){
+	public String toString() {
 		return this.database.toString();
 	}
+
 	/**
 	 * @return the database
 	 */
 	public ArrayList<Scan> getDatabase() {
 		return database;
 	}
+
 	/**
 	 * @return the hash_map
 	 */
 	public Map<String, ArrayList<Scan>> getHash_map() {
 		return hash_map;
 	}
+
 	/**
-	 * @param database the database to set
+	 * @param database
+	 *            the database to set
 	 */
 	public void setDatabase(ArrayList<Scan> database) {
 		this.database = database;
 	}
+
 	public void douplicate() {
-		Set <Scan> data =new HashSet<Scan>();
+		Set<Scan> data = new HashSet<Scan>();
 		data.addAll(this.database);
-		System.out.println(data.size());
 		this.database.addAll(data);
-		System.out.println(this.database.size());	
 	}
 }
